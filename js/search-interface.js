7e04bab91c21153e937d6d774a6b717a617e6801
Cassandra Culkins <cassy.culkins@gmail.com>
2016-03-18 16:18:53 -0700

    add jquery and javascript to add functionality to the form

---
+++ js/search-interface.js
@@ -0,0 +1,8 @@
+var apiKey = "a79875ed23632e525bcc50f6f4a3eb28f0df2522";
+
+$(document).ready(function() {
+  $('#userRepoButton').click(function() {
+    var userName = $('#userRepo').val();
+    $('#userRepo').val("");
+  });
+});
--- js/search.js
+++ js/search.js
@@ -1,9 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+$.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+  $('showRepos').text('Here is some information on' + userName + '.');
+console.log(response);
+}).fail(function(error) {
+  $('#showRepos').text(error.responseJSON.message);
+});
