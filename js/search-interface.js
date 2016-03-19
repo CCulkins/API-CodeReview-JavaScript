88d8f9298160a08c26e51915b7e3d6c3a77c7631
Cassandra Culkins <cassy.culkins@gmail.com>
2016-03-18 17:24:13 -0700

    not functioning, bootstrap error, display error

--- js/search-interface.js
+++ js/search-interface.js
@@ -4,5 +4,7 @@ $(document).ready(function() {
   $('#userRepoButton').click(function() {
     var userName = $('#userRepo').val();
     $('#userRepo').val("");
+    var fullUserInfo = getRepos(userName);
+    $('#showRepos').text(userName + fullUserInfo);
   });
 });
--- js/search.js
+++ js/search.js
@@ -1,9 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
+exports.getRepos = function(userName){
 $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
-  $('showRepos').text('Here is some information on' + userName + '.');
+  $('#showRepos').text('Here is some information on' + userName + '.');
 console.log(response);
+  var fullUserInfo = response;
+  return fullUserInfo;
 }).fail(function(error) {
   $('#showRepos').text(error.responseJSON.message);
 });
+};
